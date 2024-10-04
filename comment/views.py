311a91c189ec3036ad9502a84b1c21798f1b7576
foy4748 <faisaljfcl@gmail.com>
2024-10-05 01:56:10 +0600

    Implemented Commenting feature

--- comment/views.py
+++ comment/views.py
@@ -1,6 +1,10 @@
 # from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
+from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
+from car.models import Car
 from comment.models import Comment
 from .forms import AddCommentForm
 
@@ -12,5 +16,18 @@ class CreateComment(CreateView):
     form_class = AddCommentForm
     template_name = "comment/comment_form.html"
 
+    def get_success_url(self):
+        return reverse_lazy("car:car_detail", kwargs={"pk": self.kwargs["car_id"]})
+
     def form_valid(self, form):
+        print(self.kwargs["car_id"])
+        comment = form.save(commit=False)
+        car = get_object_or_404(Car, pk=self.kwargs["car_id"])
+        user = self.request.user
+        carOK = car is not None
+        userOK = user is not None
+        if carOK and userOK:
+            comment.user = user
+            comment.car = car
+            comment.save()
         return super().form_valid(form)
