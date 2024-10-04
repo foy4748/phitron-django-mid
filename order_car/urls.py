63de6787718f36fe8ec40f0b63407b71083a3a78
foy4748 <faisaljfcl@gmail.com>
2024-10-04 11:34:13 +0600

    Added Car Ordering Mechanism

--- order_car/urls.py
+++ order_car/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from django.views.generic import TemplateView
+
+from . import views
 
 app_name = "order_car"
 
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="base.html")),
+    path("place-order/<str:pk>/", views.PlaceOrder, name="place_order"),
+    path("my-orders/", views.OrderList.as_view(), name="my_orders"),
 ]
--- order_car/views.py
+++ order_car/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
+from django.http.response import HttpResponseRedirect
+from django.urls import reverse
+from django.views.generic import ListView
+from car.models import Car
+from order_car.models import CarOrder
+from django.contrib import messages
 
 # Create your views here.
+
+
+@login_required
+def PlaceOrder(req, pk):
+    car = Car.objects.get(pk=pk)
+    user = req.user
+    carOK = car is not None
+    userOK = user is not None
+
+    if carOK and userOK:
+        car.quantity = car.quantity - 1
+        car.save()
+        newOrder = CarOrder(buyer_id=user, car_id=car)
+        newOrder.save()
+        print(newOrder)
+        success_message = f"Place a new order: {car.brand} | {car.car_model}"
+        messages.success(req, success_message)
+        nextUrl = reverse("car:car_list")
+        return HttpResponseRedirect(nextUrl)
+    else:
+        error_message = f"Failed to order"
+        messages.error(req, error_message)
+        nextUrl = reverse("car:car_list")
+        return HttpResponseRedirect(nextUrl)
+
+
+class OrderList(LoginRequiredMixin, ListView):
+    model = CarOrder
+    template_name = "order_car/carorder_list.html"
+
+    # Thanks to this stackoverflow page
+    # https://stackoverflow.com/questions/38471260/django-filtering-by-user-id-in-class-based-listview
+
+    def get_queryset(self):
+        return CarOrder.objects.filter(buyer_id=self.request.user)
